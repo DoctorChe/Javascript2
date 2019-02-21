2aa0f4e9f41abdb37b0d9608e804f149a47fb50a
DoctorChe <duncan.reg@yandex.ru>
2019-02-22 00:31:22 +0300

    first lesson

---
+++ js/main.js
@@ -0,0 +1,24 @@
+const products = [
+    {id: 1, title: 'Notebook', price: 2000},
+    {id: 2, title: 'Mouse', price: 20},
+    {id: 3, title: 'Keyboard', price: 35},
+    {id: 4, title: 'Gamepad', price: 58},
+    {id: 5, title: 'Chair', price: 200},
+    {id: 6, title: 'Table'},
+];
+
+const renderProduct = (id, title, price = 0) => `<div class="product-item">
+                                                    <h3>${title}</h3>
+                                                    <p>${price}</p>
+                                                    <button 
+                                                    data-id="${id}"
+                                                    data-price="${price}"
+                                                    data-title="${title}"
+                                                    >Купить</button>
+                                                </div>`;
+const renderPage = list => {
+    const productsList = list.map(item => renderProduct(item.id, item.title, item.price));
+    document.querySelector('.products').innerHTML = productsList;
+};
+
+renderPage(products);
