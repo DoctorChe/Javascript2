5e9199465f6d76eb9337dd3fb3e2bcc279e61f5e
DoctorChe <duncan.reg@yandex.ru>
2019-02-26 10:14:26 +0300

    lesson-2 is done

--- js/main.js
+++ js/main.js
@@ -1,24 +1,69 @@
-const products = [
-    {id: 1, title: 'Notebook', price: 2000},
-    {id: 2, title: 'Mouse', price: 20},
-    {id: 3, title: 'Keyboard', price: 35},
-    {id: 4, title: 'Gamepad', price: 58},
-    {id: 5, title: 'Chair', price: 200},
-    {id: 6, title: 'Table'},
-];
+class ProductsList {
+    constructor(container = '.products'){
+        this.container = container;
+        this.goods = [];
+        this.allProducts = [];
+        this._fetchGoods();
+    }
+    calcTotalCost(){
+        let result = 0;
+        for (let product of this.allProducts){
+            result += product.price;
+        }
+        return result;
+    }
+    _fetchGoods(){
+        this.goods = [
+            {title: 'Notebook', price: 2000},
+            {title: 'Mouse', price: 20},
+            {title: 'Keyboard', price: 35},
+            {title: 'Gamepad', price: 48},
+            {title: 'Chair', price: 500},
+        ];
+    }
+    render(){
+        let block = document.querySelector(this.container);
+        for (let product of this.goods){
+            const productObj = new Product(product);
+            this.allProducts.push(productObj);
+            block.insertAdjacentHTML('beforeend', productObj.render());
+        }
+    }
+}
 
-const renderProduct = (id, title, price = 0) => `<div class="product-item">
-                                                    <h3>${title}</h3>
-                                                    <p>${price}</p>
-                                                    <button 
-                                                    data-id="${id}"
-                                                    data-price="${price}"
-                                                    data-title="${title}"
-                                                    >Купить</button>
-                                                </div>`;
-const renderPage = list => {
-    const productsList = list.map(item => renderProduct(item.id, item.title, item.price)).join('');
-    document.querySelector('.products').innerHTML = productsList;
-};
+class Product {
+    constructor(item, img = 'https://placehold.it/200x150'){
+        this.title = item.title;
+        this.price = item.price;
+        this.img = img;
+    }
+    render(){
+        return `<div class="product-item">
+                    <img src="${this.img}" alt="Some img">
+                    <div class="desc">
+                        <h3>${this.title}</h3>
+                        <p>${this.price} $</p>
+                    </div>
+                </div>`;
 
-renderPage(products);
+    }
+}
+
+class Basket {
+    constructor(){}
+    addItem(){}
+    removeItem(){}
+    countItems(){}
+    calcTotalPrice(){}
+    clear(){}
+    render(){}
+}
+
+class BasketItem {
+    constructor(){}
+    render(){}
+}
+
+const list = new ProductsList();
+list.render();
+// console.log(list.calcTotalCost());
