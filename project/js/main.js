46f68dca92ddb7bf6d401b584c9e56eb6a7b4546
DoctorChe <duncan.reg@yandex.ru>
2019-03-05 14:37:48 +0300

    add classes List, Item

--- project/js/main.js
+++ project/js/main.js
@@ -19,13 +19,88 @@ const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-a
 
 // getRequest('api').then()  // пример использования
 
+
+class List {
+    constructor(url, container){
+        this.container = container;
+        this.url = url;
+        this.goods = [];
+        this.allProducts = [];
+        this.filtered = [];
+        this._init();
+    }
+    getJson(url){
+        return fetch(url ? url : `${API + this.url}`)
+            .then(result => result.json())
+            .catch(error => console.log(error))
+    }
+    calcSum(){
+        return this.allProducts.reduce((accum, item) => accum += item.price, 0);
+    }
+    handleData(data){
+        this.goods = data;
+        this.render();
+    }
+    render(){
+        const block = document.querySelector(this.container);
+        for (let product of this.goods){
+            const productObj = new list[this.constructor.name](product);
+            this.allProducts.push(productObj);
+            block.insertAdjacentHTML('beforeend', productObj.render());
+        }
+    }
+    filter(value){
+        const regexp = new RegExp(value, 'i');
+        this.filtered = this.allProducts.filter(product => regexp.test(product.product_name));
+        this.allProducts.forEach(el => {
+            const block = document.querySelector(`.product-item[data-id="${el.id_product}"]`);
+            if(!this.filtered.includes(el)){
+                block.classList.add('invisible');
+            } else {
+                block.classList.remove('invisible');
+            }
+        })
+    }
+    _init(){
+        return false
+    }
+}
+class Item {
+    constructor(el, img = 'https://placehold.it/200x150'){
+        this.product_name = el.product_name;
+        this.price = el.price;
+        this.img = img;
+        this.id_product = el.id_product
+    }
+
+    render(){
+        return `<div class="product-item" data-id="${this.id_product}">
+                    <img src="${this.img}" alt="Some img">
+                    <div class="desc">
+                        <h3>${this.product_name}</h3>
+                        <p>${this.price} $</p>
+                        <button class="buy-btn"
+                        data-id="${this.id_product}"
+                        data-price="${this.price}"
+                        data-name="${this.product_name}"
+                        data-img="${this.img}">
+                        Купить
+</button>
+                    </div>
+                </div>`;
+
+    }
+}
+
+
 class ProductsList {
     constructor(container = '.products'){
         this.container = container;
         this.goods = [];
         this.allProducts = [];
         // this._fetchGoods();
-        this._getProducts().then(() => this.render());
+        // this._getProducts().then(() => this.render());
+        this._getProducts();
         // .then(data => {
         //     this.goods = data;
         //     this.render();
@@ -52,11 +127,12 @@ class ProductsList {
     // }
 
     _getProducts(){
-        return fetch(`${API}/catalogData.json`)
+        // return fetch(`${API}/catalogData.json`)
+        fetch(`${API}/catalogData.json`)
             .then(result => result.json())
             .then(data => {
                 this.goods = data;
-                // this.render()
+                this.render()
             })
             .catch(error => console.log(error))
     }
