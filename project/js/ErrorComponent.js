dd73a4179dbc54c8724bdf61efb76dacb44360f6
DoctorChe <duncan.reg@yandex.ru>
2019-03-12 16:47:52 +0300

    create template for error component

---
+++ project/js/ErrorComponent.js
@@ -0,0 +1,16 @@
+Vue.component('error', {
+    data() {
+        return {
+            isError: false
+        }
+    },
+    mounted() {
+
+    },
+    methods: {
+
+    },
+    template: `<div>
+                    <h2>Error</h2>
+               </div>`
+});
